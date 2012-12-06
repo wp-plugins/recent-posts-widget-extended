276ad1f282d3c2dcfd382a6d6c8a675a5eb7c150
satrya <satrya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-06 10:46:35 +0000

    First commit of Recent Posts Widget Extended plugin
    git-svn-id: https://plugins.svn.wordpress.org/recent-posts-widget-extended/trunk@634782 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Plugin Name ===
+Contributors: marga satrya, tokokoo
+Donate link: 
+Tags: recent posts, thumbnails, widget, widgets, sidebar
+Requires at least: 3.3
+Tested up to: 3.4.2
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Provides recent posts widget with a more advanced settings.
+
+== Description ==
+
+This plugin will enable a custom and advanced recent posts widget. Allows you to display a list of the most recent posts with thumbnail and excerpt also you can display it from all or a specific category. The recent posts widget extended uses [Transients API](http://codex.wordpress.org/Transients_API) for delivering cached to optimize your site performance when used the widget.
+
+= Features Include: =
+
+* Display thumbnails, with customizable size.
+* Display excerpt, with customizable length.
+* Display from all or a specific category.
+* Post type option.
+* Transients API
+
+= To Do: =
+* Display meta(author, comment, date)
+
+= Support: =
+Please open issue on [github issue](https://github.com/tokokoo/recent-posts-widget-extended/issues) for any support question and translation submission.
+
+== Installation ==
+
+1. Upload the 'recent-posts-widget-extended' folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+1. The widget settings
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
---
+++ rpwe.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Plugin Name: Recent Posts Widget Extended
+Plugin URI: http://tokokoo.com
+Description: Enables recent posts widget with advanced settings.
+Version: 0.1
+Author: Marga Satrya
+Author URI: http://tokokoo.com
+Author Email: satrya@tokokoo.com
+License: GPLv2
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( 'RPW_Extended' ) ) :
+
+class RPW_Extended {
+
+	/**
+	 * PHP5 constructor method.
+	 *
+	 * @since 0.1
+	 */
+	public function __construct() {
+
+		add_action( 'plugins_loaded', array( &$this, 'constants' ), 1 );
+
+		add_action( 'plugins_loaded', array( &$this, 'i18n' ), 2 );
+
+		add_action( 'plugins_loaded', array( &$this, 'includes' ), 3 );
+
+		add_action( 'init', array( &$this, 'init' ) );
+
+	}
+
+	/**
+	 * Defines constants used by the plugin.
+	 *
+	 * @since 0.1
+	 */
+	public function constants() {
+
+		define( 'RPWE_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+		define( 'RPWE_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+		define( 'RPWE_INCLUDES', RPWE_DIR . trailingslashit( 'includes' ) );
+
+	}
+
+	/**
+	 * Loads the translation files.
+	 *
+	 * @since 0.1
+	 */
+	public function i18n() {
+
+		/* Load the translation of the plugin. */
+		load_plugin_textdomain( 'rpwe', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+	}
+
+	/**
+	 * Loads the initial files needed by the plugin.
+	 *
+	 * @since 0.1
+	 */
+	public function includes() {
+
+		require_once( RPWE_INCLUDES . 'widget-recent-posts-extended.php' );
+	}
+
+	/**
+	 * Register custom style for the widget.
+	 *
+	 * @since 0.1
+	 */
+	function init() {
+		
+		if( ! is_admin() ) {
+
+			wp_enqueue_style( 'rpwe-style', RPWE_URI . 'rpwe.css' );
+
+		}
+
+	}
+
+}
+
+new RPW_Extended;
+
+endif;
